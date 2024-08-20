01f0ce503bcf493bf6896a257ffc7bb8e24f6184
Yudia Artana <yudiaartana87@gmail.com>
2024-08-20 13:44:58 +0800

    edit

--- collect_data.py
+++ collect_data.py
@@ -6,7 +6,7 @@ if not os.path.exists(DATA_DIR):
     os.makedirs(DATA_DIR)
 
 number_of_classes = 10
-dataset_size = 150
+dataset_size = 100
 
 cap = cv2.VideoCapture(0)
 for j in range(1, number_of_classes):
